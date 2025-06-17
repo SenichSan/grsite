9c3acbbe4fb3e5b86de7205522bbc6be6a71df7c
PyDjango <ay@gmail.com>
2025-06-17 19:16:31 +0300

    Templates for user app added

--- project/app/urls.py
+++ project/app/urls.py
@@ -24,12 +24,11 @@ from app import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('catalog/', include('goods.urls', namespace='catalog'))
+    path('catalog/', include('goods.urls', namespace='catalog')),
+    path('user/', include('users.urls', namespace='user')),
 ]
 if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include("debug_toolbar.urls"))
     ]
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
