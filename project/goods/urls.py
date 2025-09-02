54a18f43112bf91c4f313f50bc6bf8982312cb29
PyDjango <ay@gmail.com>
2025-09-02 14:08:31 +0300

    Допиливание страницы товара.

--- project/goods/urls.py
+++ project/goods/urls.py
@@ -7,10 +7,9 @@ app_name = 'goods'
 
 urlpatterns = [
     path('search/', views.CatalogView.as_view(), name='search'),
+    # Catalog root (all products) at /catalog/
+    path('', CatalogView.as_view(), name='catalog_all'),
     path('<slug:category_slug>/', views.CatalogView.as_view(), name='index'),
     path('product/<slug:product_slug>/', views.ProductView.as_view(), name='product'),
     path('categories/', views.CategoriesView.as_view(), name='categories'),
-    path("catalog/", CatalogView.as_view(), name="catalog_all"),
-    path("catalog/<slug:category_slug>/", CatalogView.as_view(), name="catalog_by_category"),
 ]
-
